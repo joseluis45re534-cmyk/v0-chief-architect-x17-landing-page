0149a6b4b687ea0ec7d515d67b751deec2c89c48
MOADSOUFIANE <joseluis45re534@gmail.com>
2026-02-28 16:46:44 +0000

    REMOVE JIVO

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             `,
           }}
         />
-        <script src="//code.jivosite.com/widget/5YJwlXjzAo" async></script>
+
       </head>
       <body className={`font-sans antialiased`}>
         <noscript>
